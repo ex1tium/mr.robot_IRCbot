d29c3819980db9632b0e16286cad6ede8a03c26d
ex1tium <ex1tium.code@outlook.com>
2016-07-15 01:50:00 +0300

    default config for quakenet

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
 console.log("Starting point");
+
+var client = require('coffea')({
+    host: 'irc.se.quakenet.org',
+    port: 6667, // default value: 6667
+    ssl: false, // set to true if you want to use ssl
+    ssl_allow_invalid: false, // set to true if the server has a custom ssl certificate
+    prefix: '!', // used to parse commands and emit on('command') events, default: !
+    channels: ['#datanuubit', '#bottitesti'], // autojoin channels, default: []
+    nick: 'mr_robot', // default value: 'coffea' with random number
+    username: 'mr_robot', // default value: username = nick
+    realname: 'Mr. Robot', // default value: realname = nick
+    throttling: 250 // default value: 250ms, 1 message every 250ms, disable by setting to false
+});
